42dbe812e9c641c6bad704a7d158852aac558c7b
Deborah Santillan <s.debb@yahoo.com>
2020-07-18 20:24:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,18 @@
 class Song
-  attr_accessor :name, :artist
 
-  def initialize(name, artist=nil)
-    @name = name
-    @artist = artist
+	attr_accessor :artist
+	attr_reader :name
+
+  def initialize(name)
+  	@name = name
   end
 
   def artist_name
-    self.artist ? self.artist.name : nil
+  	if self.artist
+  		self.artist.name
+  	else
+  		nil
+  	end
   end
+
 end
