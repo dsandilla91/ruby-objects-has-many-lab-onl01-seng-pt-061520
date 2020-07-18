7e208d1421b5e8d69ca2cb485de7a000574a2723
Deborah Santillan <s.debb@yahoo.com>
2020-07-18 20:20:47 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,11 @@
 class Post
+  attr_accessor :author, :title
 
-  attr_accessor :title, :author
   def initialize(title)
     @title = title
   end
 
   def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
+    self.author ? self.author.name : nil
   end
 end
